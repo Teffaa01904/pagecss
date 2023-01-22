a6e9f736ccfb9046024d630688e5db675d31aafa
Teffaa01904 <massielleo0@gmail.com>
2023-01-22 02:26:28 -0500

    Se agrega el proyecto

---
+++ Js/slider.js
@@ -0,0 +1,31 @@
+(function(){
+    const sliders = [...document.querySelectorAll('.testimony__body')];
+    const buttonNext = document.querySelector('#next');
+    const buttonBefore = document.querySelector('#before');
+    let value;
+
+    buttonNext.addEventListener('click', ()=>{
+        ChangePosition(1);
+    });
+
+    buttonBefore.addEventListener('click', ()=>{
+        ChangePosition(-1);
+    });
+
+    const ChangePosition = (add)=>{
+        const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
+        value = Number(currentTestimony);
+        value+= add;
+
+
+        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show'); 
+        if(value === sliders.length+1 || value === 0){
+            value = value === 0 ? sliders.length  : 1;
+        }
+
+        sliders[value-1].classList.add('testimony__body--show');
+
+    
+    }
+    
+})();
